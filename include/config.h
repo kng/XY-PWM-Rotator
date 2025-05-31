213a8dcf3f558670c8326aeacc91c1bb03a9d9d5
Daniel Ekman <knegge@gmail.com>
2025-05-31 13:09:23 +0200

    Move settings to config.h
    Signed-off-by: Daniel Ekman <knegge@gmail.com>

---
+++ include/config.h
@@ -0,0 +1,12 @@
+#define X_PIN 9     // pin controlling the X servo
+#define Y_PIN 10    // pin controlling the Y servo
+
+#define X_MIN 544   // calibrate the min/max for your servos
+#define X_MAX 2400  // default min is 544, max is 2400
+#define Y_MIN 544   // 500-2500 is probably close to the limit
+#define Y_MAX 2400  // make sure the servos dont hit endstop
+
+#define EL_MIN 20   // limit the minimum elevation, angle up from horizontal
+#define SPEED 20    // maximum moving speed, speed commands can limit this further
+
+#define BUF_SIZE 16 // max receiving buffer, only needs to fit one command
--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,8 @@
 #include <Arduino.h>
 #include <VarSpeedServo.h> 
+#include "config.h"
 
 // Subset of GS-232B compatible protocol implemented
-
-#define X_PIN 9     // pin controlling the X servo
-#define Y_PIN 10    // pin controlling the Y servo
-
-#define X_MIN 544   // calibrate the min/max for your servos
-#define X_MAX 2400  // default min is 544, max is 2400
-#define Y_MIN 544   // 500-2500 is probably close to the limit
-#define Y_MAX 2400  // make sure the servos dont hit endstop
-
-#define EL_MIN 20   // limit the minimum elevation, angle up from horizontal
-#define SPEED 20    // maximum moving speed, speed commands can limit this further
-
-#define BUF_SIZE 16 // max receiving buffer, only needs to fit one command
 
 VarSpeedServo servo_x, servo_y;
 char buf[BUF_SIZE], resp[BUF_SIZE], *ptr;
